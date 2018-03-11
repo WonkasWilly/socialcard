e14153cac821a008d79dbe4f9bc119fbdff92ef5
Phillip Choi <pchoi13370@gmail.com>
2018-03-11 00:12:59 -0800

    Made image responsive

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../CSS/Image.css';
 
 class Image extends React.Component {
 	render() {
 		return(
 			<div className="image-container">
-				<img src="../..images/Space.jpg" alt="Random Image"/>
+				<img src="../../images/Space.jpg" alt="Random Image"/>
 			</div>
 		)
 	}
