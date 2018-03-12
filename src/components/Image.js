5989baf6557329764bd15e42cf8bcf4d6d776154
Phillip Choi <pchoi13370@gmail.com>
2018-03-11 23:28:42 -0700

    Added the social media component to my project; adjusted the layout

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import SocialMedia from './SocialMedia';
+
 import '../CSS/Image.css';
+
 
 class Image extends React.Component {
 	render() {
 		return(
 			<div className="image-container">
 				<img src="../../images/Space.jpg" alt="Space"/>
+				<SocialMedia />
 			</div>
 		)
 	}
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../CSS/SocialMedia.css';
+
+class SocialMedia extends Component {
+	render() {
+		return(
+			<div className='social-container'>
+				<h1>Social Media Here!</h1>
+			</div>
+		)
+	}
+}
+
+export default SocialMedia;
